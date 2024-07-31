e31b656c684359f3e298c78c9aabf30984135742
Andrew Martin <andrew.thaddeus@gmail.com>
2024-07-31 15:08:43 -0400

    Add encodeText and encodeShortText for ApiKey

--- src/Kafka/ApiKey.hs
+++ src/Kafka/ApiKey.hs
@@ -1,10 +1,14 @@
 {-# language DerivingStrategies #-}
 {-# language GeneralizedNewtypeDeriving #-}
+{-# language OverloadedStrings #-}
 {-# language PatternSynonyms #-}
 
 module Kafka.ApiKey
   ( -- * Type
     ApiKey(..)
+    -- * Encoding
+  , encodeText
+  , encodeShortText
     -- * Patterns
   , pattern Produce
   , pattern Fetch
@@ -57,12 +61,87 @@ module Kafka.ApiKey
   ) where
 
 import Data.Int (Int16)
+import Data.Text (Text)
+import Data.Text.Short (ShortText)
+
+import qualified Data.Text.Short as TS
+
 
 -- | An ApiKey. This is given its own type because it improves a lot
 -- of derived 'Show' instances and because it makes several type
 -- signatures more clear.
 newtype ApiKey = ApiKey Int16
   deriving newtype (Eq)
+
+encodeText :: ApiKey -> Text
+{-# inline encodeText #-}
+encodeText = TS.toText . encodeShortText
+
+encodeShortText :: ApiKey -> ShortText
+encodeShortText (ApiKey k) = case k of
+  0  -> "Produce"
+  1  -> "Fetch"
+  2  -> "ListOffsets"
+  3  -> "Metadata"
+  4  -> "LeaderAndIsr"
+  5  -> "StopReplica"
+  6  -> "UpdateMetadata"
+  7  -> "ControlledShutdown"
+  8  -> "OffsetCommit"
+  9  -> "OffsetFetch"
+  10 -> "FindCoordinator"
+  11 -> "JoinGroup"
+  12 -> "Heartbeat"
+  13 -> "LeaveGroup"
+  14 -> "SyncGroup"
+  15 -> "DescribeGroups"
+  16 -> "ListGroups"
+  17 -> "SaslHandshake"
+  18 -> "ApiVersions"
+  19 -> "CreateTopics"
+  20 -> "DeleteTopics"
+  21 -> "DeleteRecords"
+  22 -> "InitProducerId"
+  23 -> "OffsetForLeaderEpoch"
+  24 -> "AddPartitionsToTxn"
+  25 -> "AddOffsetsToTxn"
+  26 -> "EndTxn"
+  27 -> "WriteTxnMarkers"
+  28 -> "TxnOffsetCommit"
+  29 -> "DescribeAcls"
+  30 -> "CreateAcls"
+  31 -> "DeleteAcls"
+  32 -> "DescribeConfigs"
+  33 -> "AlterConfigs"
+  34 -> "AlterReplicaLogDirs"
+  35 -> "DescribeLogDirs"
+  36 -> "SaslAuthenticate"
+  37 -> "CreatePartitions"
+  38 -> "CreateDelegationToken"
+  39 -> "RenewDelegationToken"
+  40 -> "ExpireDelegationToken"
+  41 -> "DescribeDelegationToken"
+  42 -> "DeleteGroups"
+  43 -> "ElectLeaders"
+  44 -> "IncrementalAlterConfigs"
+  45 -> "AlterPartitionReassignments"
+  46 -> "ListPartitionReassignments"
+  47 -> "OffsetDelete"
+  48 -> "DescribeClientQuotas"
+  49 -> "AlterClientQuotas"
+  50 -> "DescribeUserScramCredentials"
+  51 -> "AlterUserScramCredentials"
+  55 -> "DescribeQuorum"
+  56 -> "AlterPartition"
+  57 -> "UpdateFeatures"
+  58 -> "Envelope"
+  60 -> "DescribeCluster"
+  61 -> "DescribeProducers"
+  64 -> "UnregisterBroker"
+  65 -> "DescribeTransactions"
+  66 -> "ListTransactions"
+  67 -> "AllocateProducerIds"
+  _ -> TS.pack (show k)
 
 instance Show ApiKey where
   showsPrec _ (ApiKey k) s = case k of
